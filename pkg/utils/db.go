4b969372d63ba57e84bd92cfd5f2b019e060362c
Duc Bui Minh <ducbm95@gmail.com>
2023-04-20 22:03:35 +0700

    update progress file

---
+++ pkg/utils/db.go
@@ -0,0 +1,113 @@
+package utils
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+type RowValues struct {
+	values         map[string]interface{}
+	columnPointers []interface{}
+	columnNames    []string
+}
+
+func NewRowVals(rows *sql.Rows) (*RowValues, error) {
+	columnNames, err := rows.Columns()
+	if err != nil {
+		return nil, fmt.Errorf("get columns: [%w]", err)
+	}
+
+	r := &RowValues{
+		values:         make(map[string]interface{}),
+		columnPointers: make([]interface{}, len(columnNames)),
+		columnNames:    columnNames,
+	}
+
+	columns := make([]interface{}, len(columnNames))
+	for i := range columns {
+		r.columnPointers[i] = &columns[i]
+	}
+
+	return r, nil
+}
+
+func (r *RowValues) Values() map[string]interface{} {
+	return r.values
+}
+
+func (r *RowValues) StringValues() map[string]string {
+	res := make(map[string]string, len(r.columnNames))
+	for _, v := range r.columnNames {
+		res[v] = r.GetValue(v)
+	}
+
+	return res
+}
+
+func (r *RowValues) Parse(rows *sql.Rows) error {
+	r.values = make(map[string]interface{})
+
+	if err := rows.Scan(r.columnPointers...); err != nil {
+		return fmt.Errorf("rows scan: [%w]", err)
+	}
+
+	for i, colName := range r.columnNames {
+		val := r.columnPointers[i].(*interface{})
+		r.values[colName] = *val
+	}
+
+	return nil
+}
+
+// func(r *RowValues)
+func (r *RowValues) GetValue(columnName string) string {
+	// return fmt.Sprintf("%v", r.values[columnName])
+	if val, ok := r.values[columnName].([]byte); ok {
+		return string(val)
+	}
+
+	return fmt.Sprintf("%v", r.values[columnName])
+}
+
+func CheckCount(rows *sql.Rows) (rowCount int) {
+	for rows.Next() {
+		err := rows.Scan(&rowCount)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "err7", err)
+			log.Fatalln(err)
+		}
+	}
+
+	return rowCount
+}
+
+func ExtractWhereClause(query string, params []interface{}) string {
+	paramValueCount := strings.Count(query, "?")
+	for count := 0; count < paramValueCount; count++ {
+		query = strings.Replace(query, "?", fmt.Sprintf("'%s'", params[count]), 1)
+	}
+
+	pos := strings.Index(query, "WHERE")
+	if pos == -1 {
+		return "WHERE 1=1"
+	}
+
+	return query[pos:]
+}
+
+// Returns the replaced column in the query.
+func ReplaceColumnValueInQuery(orignal_query string, col_name string, col_value string) string {
+	updated_query := ""
+	if strings.Index(orignal_query, " AND "+col_name+"=''") > 0 {
+		// If the column value was empty
+		updated_query = strings.Replace(orignal_query, " AND "+col_name+"=''", " AND "+col_name+"='"+col_value+"'", 1)
+	} else {
+		// If the column value was not empty
+		sub_string := GetStringBetween(orignal_query, " AND "+col_name+"='", "'")
+		updated_query = strings.Replace(orignal_query, sub_string, col_value, 1)
+	}
+	return updated_query
+}
--- progress.md
+++ progress.md
@@ -1,3 +1,14 @@
+Thursday, 2023/04/19
+- Refactor transfer service (done). This service now has a better structure:  
+	- 3 layers
+	- Some function move to pkgs for reusing in the future.
+
+Wenesday, 2023/04/19
+- Refactor transfer service (in progress)
+
+Monday, 2023/04/17
+- Refactor cell_url and simple_curd done (3 layer and use components db from pkgs)
+- 
 Thurday, 2023/04/13
 - Integrating and refactoring cell_url app.  
 	+ using db component from pkgs app   
@@ -5,5 +16,3 @@ Thurday, 2023/04/13
 	+ reorganise folder structure  
 	+ reorganise code structure (3 layer: handler, service and repository)  
 
-Monday, 2023/04/13
-- Refactor cell_url and simple_curd done (3 layer and use components db from pkgs)
