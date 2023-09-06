4161ea82d7318c961305d6d90a793b7d1bdb1559
wilson <wilson_wz@163.com>
2023-09-06 18:55:27 +0800

    Send registration email;Verification email

---
+++ pkg/gip/rest_api.go
@@ -0,0 +1,74 @@
+package gip
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"time"
+)
+
+// Send registration email
+func SendRegistrationEmail(email string) error {
+	url := "https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=%s"
+	url = fmt.Sprintf(url, os.Getenv("GOOGLE_API_KEY"))
+	data := map[string]interface{}{
+		"requestType": "EMAIL_SIGNIN",
+		"email":       email,
+		"continueUrl": "http://bynar-397714.firebaseapp.com/signup",
+	}
+	jsonByte, _ := json.Marshal(data)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonByte))
+	if err != nil {
+		return err
+	}
+	defer req.Body.Close()
+
+	client := &http.Client{
+		Timeout: 20 * time.Second,
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	response, _ := ioutil.ReadAll(res.Body)
+	if res.StatusCode != 200 {
+		return errors.New(string(response))
+	}
+
+	return nil
+}
+
+// Verification email, invalid for type 'EMAIL SIGNIN'
+func VerificationEmail(oobCode string) error {
+	url := "https://identitytoolkit.googleapis.com/v1/accounts:update?key=%s"
+	url = fmt.Sprintf(url, os.Getenv("GOOGLE_API_KEY"))
+	data := map[string]interface{}{
+		"oobCode": oobCode,
+	}
+	jsonByte, _ := json.Marshal(data)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonByte))
+	if err != nil {
+		return err
+	}
+	defer req.Body.Close()
+
+	client := &http.Client{
+		Timeout: 20 * time.Second,
+	}
+	res, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	response, _ := ioutil.ReadAll(res.Body)
+	if res.StatusCode != 200 {
+		return errors.New(string(response))
+	}
+
+	return nil
+}
