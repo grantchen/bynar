79ffd3d3984fa40320d932e383b5f97ffbba9782
Jack Shih <develop@kshih.com>
2023-09-07 13:26:04 +0800

    remove unused code in rende pkg and fix the create user repo

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -4,33 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-	"math"
 	"net/http"
 	"reflect"
 )
-
-type PaginationResponse struct {
-	CurrentPage int `json:"current_page"`
-	TotalPages  int `json:"total_pages"`
-	TotalCount  int `json:"total_count"`
-	PerPage     int `json:"per_page"`
-}
-
-func newPagination(currentPage, totalCount, perPage int) *PaginationResponse {
-	return &PaginationResponse{
-		CurrentPage: currentPage,
-		TotalCount:  totalCount,
-		PerPage:     perPage,
-		TotalPages:  int(math.Ceil(float64(totalCount) / float64(perPage))),
-	}
-}
-
-type Response struct {
-	Code       int                 `json:"code"`
-	Message    string              `json:"message"`
-	Data       interface{}         `json:"data,omitempty"`
-	Pagination *PaginationResponse `json:"pagination,omitempty"`
-}
 
 func DecodeJSON(r io.Reader, v interface{}) error {
 	data, err := io.ReadAll(r)
@@ -69,10 +45,6 @@ func Ok(w http.ResponseWriter, v interface{}) {
 
 func Error(w http.ResponseWriter, msg string) {
 	http.Error(w, msg, http.StatusInternalServerError)
-}
-
-func Pagination(w http.ResponseWriter, v []interface{}, currentPage, totalCount, perPage int) {
-	renderJSON(w, Response{Code: 200, Message: "OK", Data: v, Pagination: newPagination(currentPage, totalCount, perPage)})
 }
 
 func renderJSON(w http.ResponseWriter, v interface{}) {
--- service/accounts/internal/repository/signup.go
+++ service/accounts/internal/repository/signup.go
@@ -29,17 +29,19 @@ func (r *accountRepositoryHandler) CreateOrganization(tx *sql.Tx, description, v
 		return 0, err
 	}
 	// Insert organization info to db
-	err = tx.QueryRow(
-		`INSERT INTO organizations (description, vat_number, country, organization_uuid, status, verified) VALUES (?, ?, ?, ?, ?, ?);SELECT LAST_INSERT_ID();`,
+	res, err := tx.Exec(
+		`INSERT INTO organizations (description, vat_number, country, organization_uuid, status, verified) VALUES (?, ?, ?, ?, ?, ?);`,
 		description, vat, country, uuid.New().String(), 1, 1,
-	).Scan(&organizationID)
+	)
 	if err != nil {
 		tx.Rollback()
 		return 0, err
 	}
+	newOrganizationID, _ := res.LastInsertId()
+	organizationID = int(newOrganizationID)
 	// Add relationship between the account and organization
 	_, err = tx.Exec(
-		`INSERT INTO organization_accounts (organization_id, organization_user_uid, organization_user_id, oraginzation_main_account) VALUES (?, ?, ?, ?)`,
+		`INSERT INTO oraginzation_accounts (organization_id, organization_user_uid, organization_user_id, oraginzation_main_account) VALUES (?, ?, ?, ?)`,
 		organizationID, uid, accountID, 1,
 	)
 	if err != nil {
@@ -54,13 +56,43 @@ func (r *accountRepositoryHandler) CreateTenantManagement(tx *sql.Tx, region str
 	var tanantID int
 	var organizations int
 	var organizationsAllowed int
-	err := r.db.QueryRow("SELECT id, oraginzations, organizations_allowed FROM tenants WHERE region = ?", region).Scan(&tanantID, &organizations, organizationsAllowed)
+	err := tx.QueryRow("SELECT id, organizations, organizations_allowed FROM tenants WHERE region = ?", region).Scan(&tanantID, &organizations, &organizationsAllowed)
 	if err != nil || organizations >= organizationsAllowed {
+		logrus.Error("create tenant error: ", err)
 		return errors.New("not allowed to insert tenants")
 	}
 	_, err = tx.Exec(
 		`INSERT INTO tenants_management (organization_id, tenant_id, status, suspended) VALUES (?, ?, ?, ?)`,
 		organizationID, tanantID, 1, 0,
+	)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	_, err = tx.Exec(
+		`UPDATE tenants SET organizations = ? WHERE id = ?`,
+		organizations+1, tanantID,
+	)
+	if err != nil {
+		tx.Rollback()
+		return err
+	}
+	return nil
+}
+
+func (r *accountRepositoryHandler) CreateCard(tx *sql.Tx, customerID, sourceID string, userID int) error {
+	var count int
+	err := tx.QueryRow(`SELECT COUNT(*) FROM accounts_cards WHERE user_id = ?`, userID).Scan(&count)
+	if err != nil {
+		return err
+	}
+	isDefault := 1
+	if count > 0 {
+		isDefault = 0
+	}
+	_, err = tx.Exec(
+		`INSERT INTO accounts_cards (user_payment_gateway_id, user_id, status, is_default, source_id, account_id) VALUES (?, ?, ?, ?, ?, ?)`,
+		customerID, userID, 1, isDefault, sourceID, userID,
 	)
 	if err != nil {
 		tx.Rollback()
@@ -75,26 +107,23 @@ func (r *accountRepositoryHandler) CreateUser(uid, email, fullName, country, add
 	if err != nil {
 		return err
 	}
-	var userID int
-	err = tx.QueryRow(
-		`INSERT INTO accounts (email, full_name, address, address_2, phone, city, postal_code, country, state, status, uid, org_id, verified) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?);SELECT LAST_INSERT_ID();`,
-		email, fullName, addressLine, addressLine2, phoneNumber, city, postalCode, country, state, 1, uid, nil, 1).Scan(&userID)
-	if err != nil {
-		logrus.Errorf("CreateUser: error: %v", err)
-		return err
-	}
-	organizationID, err := r.CreateOrganization(tx, organizationName, vat, organisationCountry, uid, userID)
-	if err != nil {
-		logrus.Errorf("CreateUser: error: %v", err)
-		return err
-	}
-	_, err = tx.Exec(
-		`INSERT INTO account_cards (user_payment_gateway_id, user_id, status, is_default, source_id, account_id) VALUES (?, ?, ?, ?, ?, ?)`,
-		customerID, sourceID, 1, 1, 1, userID,
+	res, err := tx.Exec(
+		`INSERT INTO accounts (email, full_name, address, address_2, phone, city, postal_code, country, state, status, uid, org_id, verified) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)`,
+		email, fullName, addressLine, addressLine2, phoneNumber, city, postalCode, country, state, 1, uid, nil, 1,
 	)
 	if err != nil {
 		logrus.Errorf("CreateUser: error: %v", err)
-		tx.Rollback()
+		return err
+	}
+	userID, _ := res.LastInsertId()
+	organizationID, err := r.CreateOrganization(tx, organizationName, vat, organisationCountry, uid, int(userID))
+	if err != nil {
+		logrus.Errorf("CreateUser: error: %v", err)
+		return err
+	}
+	err = r.CreateCard(tx, customerID, sourceID, int(userID))
+	if err != nil {
+		logrus.Errorf("CreateUser: error: %v", err)
 		return err
 	}
 	err = r.CreateTenantManagement(tx, state, organizationID)
@@ -102,5 +131,5 @@ func (r *accountRepositoryHandler) CreateUser(uid, email, fullName, country, add
 		logrus.Errorf("CreateUser: error: %v", err)
 		return err
 	}
-	return nil
+	return tx.Commit()
 }
