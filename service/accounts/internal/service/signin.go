3aaaa46708faa5f373c65119c50dc38fc13b8a18
dongjs <dongjs@tajansoft.com>
2023-09-11 17:04:38 +0800

    refactor return error info to user

--- service/accounts/internal/service/signin.go
+++ service/accounts/internal/service/signin.go
@@ -3,32 +3,39 @@ package service
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"git-codecommit.eu-central-1.amazonaws.com/v1/repos/accounts/internal/model"
 	"git-codecommit.eu-central-1.amazonaws.com/v1/repos/pkgs/gip"
+	"github.com/sirupsen/logrus"
 	"os"
 )
 
 // SignIn is a service method which handles the logic of user login
 func (s *accountServiceHandler) SignIn(email, oobCode string) (idToken string, err error) {
 	if err = s.VerifyEmail(email); err != nil {
-		return "", err
+		logrus.Errorf("SignIn: verify email error: %+v", err)
+		return "", fmt.Errorf("email is not signed up")
 	}
 	account, err := s.ar.SelectSignInColumns(email)
 	if err != nil || account == nil {
-		return "", fmt.Errorf("SignIn: %s no user selected", email)
+		logrus.Errorf("SignIn: %s no user selected", email)
+		return "", fmt.Errorf("sign in failed")
 	}
 	err = gip.SignInWithEmailLink(email, oobCode)
 	if err != nil {
-		return "", fmt.Errorf("SignIn: verification oobCode err: %+v", err)
+		logrus.Errorf("SignIn: verification oobCode err: %+v", err)
+		return "", fmt.Errorf("sign in failed")
 	}
 	claims, err := convertSignInToClaims(account)
 	if err != nil {
-		return "", err
+		logrus.Errorf("SignIn: convert sign in claims err: %+v", err)
+		return "", fmt.Errorf("sign in failed")
 	}
 	token, err := s.authProvider.SignIn(context.Background(), account.Uid, claims)
 	if err != nil {
-		return "", fmt.Errorf("SignIn: %s generate idtoke err: %+v", email, err)
+		logrus.Errorf("SignIn: %s generate idtoke err: %+v", email, err)
+		return "", fmt.Errorf("sign in failed")
 	}
 	return token, nil
 
@@ -51,9 +58,14 @@ func convertSignInToClaims(signIn *model.SignIn) (map[string]interface{}, error)
 // SendSignInEmail send google identify platform with oobCode for sign in
 func (s *accountServiceHandler) SendSignInEmail(email string) error {
 	if err := s.VerifyEmail(email); err != nil {
-		return err
+		logrus.Errorf("SendSignInEmail: verify email error: %+v", err)
+		return errors.New("email is not signed up")
 	}
-	return gip.SendRegistrationEmail(email, os.Getenv("SIGNIN_REDIRECT_URL"))
+	if err := gip.SendRegistrationEmail(email, os.Getenv("SIGNIN_REDIRECT_URL")); err != nil {
+		logrus.Errorf("SendSignInEmail: send registration email error: %+v", err)
+		return errors.New("email sending failed")
+	}
+	return nil
 }
 
 // VerifyEmail check email is stored in db and google identify platform
