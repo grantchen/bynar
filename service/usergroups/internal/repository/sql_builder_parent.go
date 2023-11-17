e8ec14f3cc01a0124ca1d9dd021fbefa494759f9
xander <xandercheung0@gmail.com>
2023-11-17 16:47:42 +0800

    fix(user_groups): parent data Count error due to dirty data

--- service/usergroups/internal/repository/sql_builder_parent.go
+++ service/usergroups/internal/repository/sql_builder_parent.go
@@ -9,14 +9,18 @@ const (
 		`
 
 	// QueryParent is a query for getting parent rows
+	// TODO remove JOIN users and delete user_group_lines when delete user
 	QueryParent = `
 		SELECT user_groups.id,
 			   user_groups.code,
 			   user_groups.description,
 			   user_groups.status,
-			   COUNT(user_group_lines.id) AS Count
+			   COUNT(lines_t.id) AS Count
 		FROM user_groups
-				 LEFT JOIN user_group_lines ON user_group_lines.parent_id = user_groups.id
+				 LEFT JOIN (SELECT user_group_lines.id AS id, user_group_lines.parent_id AS parent_id
+							FROM user_group_lines
+									 INNER JOIN users ON user_group_lines.user_id = users.id) lines_t
+						   ON lines_t.parent_id = user_groups.id
 		WHERE 1=1
 		GROUP BY user_groups.id
 		`
